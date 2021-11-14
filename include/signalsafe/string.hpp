974e63577b98e4e31e1859027982ff0bd4a63740
Joshua Gerrard <joshuagerrard@protonmail.com>
2021-11-14 15:46:03 +0000

    Document format.

--- include/signalsafe/string.hpp
+++ include/signalsafe/string.hpp
@@ -4,6 +4,15 @@
 #include <span>
 
 namespace signalsafe::string {
+    //!
+    //! \brief  Formats a string, where % delimits where to place each provided argument.
+    //!
+    //! \param[in]    The format string.
+    //! \param[out]   Where to write the formatted string.
+    //! \paran[args]  The args the use when formatting.
+    //!
+    //! \returns  The number of bytes written.
+    //!
     template <typename... ArgTypes>
     inline std::size_t format(
         std::span<const char> format,
